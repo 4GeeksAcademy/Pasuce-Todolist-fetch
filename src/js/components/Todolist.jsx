aa2892f4e3438d755ca724a1fcd452a5b3eb5074
Pa-ola28 <paolasuarezceron1@gmail.com>
2025-07-19 12:05:40 +0000

    Terminado

--- src/js/components/Todolist.jsx
+++ src/js/components/Todolist.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Todolist = () => {
@@ -15,25 +15,75 @@ export const Todolist = () => {
     const handleKeyUp = (e) => {
 
         if (e.key === "Enter" && inputValue.trim() !== "") {
-            setTareas([...tareas, inputValue.trim()])
+            crearTarea(inputValue.trim())
             setInputValue("")
         }
     }
 
-    const eliminar = (index) => {
-        const nuevoArreglo = tareas.filter((tarea, i) => {
-            return i !== index
+    const eliminarTarea = async (id) => {
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: "DELETE"
         })
+        getTareasFromApi()
 
-        setTareas(nuevoArreglo)
 
     }
+
+    const crearTarea = async (tarea) => {
+        const response = await fetch("https://playground.4geeks.com/todo/todos/user_paola", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                label: tarea,
+                is_done: false
+            })
+        })
+        const data = response.json()
+        getTareasFromApi()
+
+    }
+
+    const getTareasFromApi = async () => {
+        const response = await fetch("https://playground.4geeks.com/todo/users/user_paola")
+        if (!response.ok) {
+            crearUsuario()
+            return
+        }
+        const data = await response.json()
+        setTareas(data.todos)
+
+    }
+
+
+
+    const borrarUsuario = async () => {
+        const response = await fetch("https://playground.4geeks.com/todo/users/user_paola", {
+            method: "DELETE"
+        })
+        crearUsuario()
+
+    }
+
+    const crearUsuario = async () => {
+        const response = await fetch("https://playground.4geeks.com/todo/users/user_paola", {
+            method: "POST"
+        })
+        const data = await response.json()
+        getTareasFromApi()
+    }
+
+    useEffect(() => {
+        getTareasFromApi();
+
+    }, [])
 
     return (
         <>
             <div className="gran-contenedor">
-                <h1>TAREAS</h1>
-                <label className="form-label" htmlFor="name">LISTA DE TAREAS:</label>
+                <h1 className="titulo-principal">LISTA DE TAREAS</h1>
+                <label className="lista-tareas" htmlFor="name">Tareas:</label>
                 <input className="form-control"
                     name="name"
                     type="text"
@@ -45,14 +95,21 @@ export const Todolist = () => {
                 {
                     tareas.map((tarea, index) => (
                         <div key={index} className="tarea-item">
-                            <span>{tarea}</span>
-                            <span className="boton-eliminar" onClick={() => eliminar(index)}>❎</span>
+                            <span>{tarea.label}</span>
+
+                            <button className="btn-outline-danger" onClick={() => eliminarTarea(tarea.id)}>
+                                <i className="bi bi-journal" style={{ fontSize: "1rem", marginRight: "2px" }}></i> Eliminar
+                            </button>
+
                         </div>
                     ))
                 }
 
 
-
+                <button className="btn" onClick={() => borrarUsuario()}>
+                    <i className="bi bi-trash3-fill"></i>
+                    <i className="animation"></i>BORRAR TODO<i className="animation"></i>
+                </button>
 
             </div>
         </>
